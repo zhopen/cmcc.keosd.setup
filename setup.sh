8ec6ffe19b79660c2246a1c9549ee34e03c482ab
zhopen <you@example.com>
2020-07-17 15:09:03 +0800

    modifyed setup.sh

--- setup.sh
+++ setup.sh
@@ -11,23 +11,14 @@ then
    exit 0
 fi
 
-DATA_DIR=/data/keosd/data-dir
-
-docker run \
-   --network host \
-   --name $CONTAINER_NAME \
-   --volume $HOST_DATA_DIR/:$DATA_DIR \
-   --detach \
-   $IMAGE   \
-   /bin/bash -c \
-   "keosd --data-dir $DATA_DIR --http-server-address $HTTP_SERVER_ADDRESS --http-validate-host false --unlock-timeout 9999999"
-
+./start.sh
+ 
 sleep 2 
 
 cleos="docker exec -it $CONTAINER_NAME cleos --wallet-url http://$HTTP_SERVER_ADDRESS"
 
 ##Create wallet 
-$cleos wallet create --file $DATA_DIR/walletpasswd.txt
+$cleos wallet create --file /data/keosd/data-dir/walletpasswd.txt
 sleep 4
 $cleos wallet open
 $cleos wallet list
